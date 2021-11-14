f1e6ab27446e26e7f993cf2acb41471c55647cd2
손채건 <thscorjs1002@naver.com>
2021-11-15 08:10:16 +0900

    Update kdt.sol

--- kdt.sol
+++ kdt.sol
@@ -4,26 +4,19 @@ pragma experimental ABIEncoderV2;
 contract KDT is KIP7Mintable, KIP7Burnable, KIP7Pausable, KIP7Metadata {
     
     uint256 public msgIdx;
-    enum Phase { Init, Pending, Done }
     
     struct Sponsor {
         address artist;
-        uint256 amount;
-        Phase state;
     }
     
     struct Message {
         string question;
         string answer;
+        uint256 amount;
     }
     
     mapping(address => Sponsor) sponsors;
     mapping(uint256 => Message) messages;
-    
-    modifier validPhase(Phase reqPhase) {
-        require(sponsors[msg.sender].state == reqPhase);
-        _;
-    }
     
     constructor(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) KIP7Metadata(name, symbol, decimals) payable public {
         _mint(msg.sender, initialSupply);
@@ -31,30 +24,24 @@ contract KDT is KIP7Mintable, KIP7Burnable, KIP7Pausable, KIP7Metadata {
     
     function registerSponser(address artist, string memory question, uint256 amount) public {
         sponsors[msg.sender] = Sponsor({
-            artist: artist,
-            amount: amount,
-            state: Phase.Pending
+            artist: artist
         });
         
         _transfer(msg.sender, address(this), amount * 1000000000000000000);
-        messages[msgIdx++].question = question;
+        messages[msgIdx].question = question;
+        messages[msgIdx++].amount = amount;
     }
     
     function answerArtist(address sponsor, string memory answer, uint256 idx) public {
-        require(sponsors[sponsor].state == Phase.Pending);
         require(sponsors[sponsor].artist == msg.sender);
-        
+        require(bytes(messages[idx].answer).length == 0);
         messages[idx].answer = answer;
-        sponsors[sponsor].state = Phase.Done;
         
-        _transfer(address(this), msg.sender, sponsors[sponsor].amount * 1000000000000000000);
-        sponsors[sponsor].amount = 0;
+        _transfer(address(this), msg.sender, messages[idx].amount * 1000000000000000000);
     }
     
-    function resetSponser() public validPhase(Phase.Done) {
+    function resetSponser() public {
         sponsors[msg.sender].artist = address(0);
-        sponsors[msg.sender].amount = 0;
-        sponsors[msg.sender].state = Phase.Init;
     }
     
     function getMessage(uint256 idx) public view returns (string memory, string memory) {
